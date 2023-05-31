0c31a3e0ccdec84bfcd441a31f0e676afdf53446
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-05-31 21:59:27 +0530

    lockfile not needed

--- src/App.svelte
+++ src/App.svelte
@@ -2,17 +2,42 @@
   import { onMount } from "svelte";
 
   import { EditorView, basicSetup } from "codemirror";
-  import { javascript } from "@codemirror/lang-javascript";
+  // import { javascript } from "@codemirror/lang-javascript";
 
-  let editor = new EditorView({
-    extensions: [basicSetup, javascript()],
-    parent: document.body,
+  let editor;
+  onMount(() => {
+    editor = new EditorView({
+      extensions: [
+        basicSetup,
+        // javascript()
+      ],
+      parent: document.querySelector("#editor"),
+    });
   });
 </script>
 
-<main class="f">
-  <iframe src="#" frameborder="0" />
+<main class="f fw">
+  <div id="editor" />
+  <iframe src="#" frameborder="0" title="Editor Output" />
 </main>
 
 <style>
+  main {
+    width: 100vw;
+    height: 100vh;
+    background: #888;
+  }
+  #editor,
+  iframe {
+    width: 50%;
+    height: 100%;
+    background: #ccc;
+  }
+  @media (max-width: 768px) {
+    #editor,
+    iframe {
+      width: 100%;
+      height: 50%;
+    }
+  }
 </style>
