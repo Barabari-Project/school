ce065bc00d38614883f1c41d4143354ca65669b4
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-05-31 21:45:31 +0530

    init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  import { EditorView, basicSetup } from "codemirror";
+  import { javascript } from "@codemirror/lang-javascript";
+
+  let editor = new EditorView({
+    extensions: [basicSetup, javascript()],
+    parent: document.body,
+  });
+</script>
+
+<main class="f">
+  <iframe src="#" frameborder="0" />
+</main>
+
+<style>
+</style>
