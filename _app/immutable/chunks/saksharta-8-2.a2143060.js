4202e1f9c14ed1e115a0f50592132b12c9a425b6
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-07-21 04:10:53 +0530

    Updates

---
+++ _app/immutable/chunks/saksharta-8-2.a2143060.js
@@ -0,0 +1,40 @@
+const t=`<section data-auto-animate>
+<h1>Project Time</h1>
+<h2>Making a Profile Page!</h2>
+</section>
+<section data-auto-animate>
+<h1>Try to make this!</h1>
+<img class="p10 rx10" src="https://i.imgur.com/QOxZ1bD.png" height="400px" alt="demo" style="width:auto;" />
+<div class="tl mx-a m10">
+  <p>
+    <b>&lt;b&gt;</b> The <b>bold</b> tag is used to give text emphasis. It makes text inside look thicker and
+    darker
+  </p>
+  <p>
+    <b>&lt;i&gt;</b> The <i>italic</i> makes text inside look slanted/oblique
+  </p>
+  <p>
+    <b>&lt;u&gt;</b> The <u>underline</u> tag is used to underline text, a bit like in textbooks for important
+    sentences
+  </p>
+  <p>
+    <b>&lt;mark&gt;</b> The <mark>mark</mark> tag is used to highlight text, a bit like in textbooks for
+    important sentences
+  </p>
+</div>
+</section>
+<section data-auto-animate>
+<h1>Now try adding an image to your profile!</h1>
+[INSERT IMAGE HERE]
+<div class="tl mx-a w-50">
+  <code>&lt;img src="..." height="333"/&gt;</code> <br>
+  <b>&lt;img&gt;</b> We use this tag to insert an image into the webpage.
+  The 'src' attribute specifies the source link of the image. The height attribute sets the height
+  of the image to 333 pixels. So, in this case, it displays an image with the provided URL and a height of 333
+  pixels.
+</div>
+</section>
+<!-- SLIDE 2 -->
+<section data-auto-animate show code>
+hi
+</section>`;export{t as default};
